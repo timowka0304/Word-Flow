2d8884a59fc6c967f7ccc6e5c3e6a9319d4d5e8d
timowka0304 <timoti20@mail.ru>
2019-04-25 00:17:00 +0700

    Created MainMenu

---
+++ Word-Flow/mainmenu.cpp
@@ -0,0 +1,60 @@
+#include "mainmenu.h"
+#include "ui_mainmenu.h"
+#include <QMessageBox>
+
+MainMenu::MainMenu(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::MainMenu)
+{
+    nounsmenu = new NounsMenu;
+    connect(nounsmenu, &NounsMenu::MainMenu, this, &MainMenu::show);
+    adjmenu = new AdjMenu;
+    connect(adjmenu, &AdjMenu::MainMenu, this, &MainMenu::show);
+    verbsmenu = new VerbsMenu;
+    connect(verbsmenu, &VerbsMenu::MainMenu, this, &MainMenu::show);
+    ui->setupUi(this);
+}
+
+MainMenu::~MainMenu()
+{
+    delete ui;
+}
+
+void MainMenu::on_exit_button_clicked()
+{
+    QMessageBox msgBox;
+    msgBox.setText("Вы уверены, что хотите выйти из приложения?");
+    msgBox.setIcon(QMessageBox::Question);
+    msgBox.setStandardButtons(QMessageBox::Yes | QMessageBox::No);
+    msgBox.setDefaultButton(QMessageBox::Yes);
+    int ret = msgBox.exec();
+       switch (ret) {
+       case QMessageBox::No:
+         msgBox.close();
+         break;
+      case QMessageBox::Yes:
+         this->close();
+         break;
+      default:
+         msgBox.close();
+         break;
+    }
+}
+
+void MainMenu::on_nounc_category_button_clicked()
+{
+    this->close();
+    nounsmenu->show();
+}
+
+void MainMenu::on_adjectives_catogory_button_clicked()
+{
+    this->close();
+    adjmenu->show();
+}
+
+void MainMenu::on_verbs_category_button_clicked()
+{
+    this->close();
+    verbsmenu->show();
+}
---
+++ Word-Flow/mainmenu.h
@@ -0,0 +1,37 @@
+#ifndef MAINMENU_H
+#define MAINMENU_H
+
+#include <QDialog>
+#include "nounsmenu.h"
+#include "adjmenu.h"
+#include "verbsmenu.h"
+
+namespace Ui {
+class MainMenu;
+}
+
+class MainMenu : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit MainMenu(QWidget *parent = nullptr);
+    ~MainMenu();
+
+private slots:
+    void on_exit_button_clicked();
+
+    void on_nounc_category_button_clicked();
+
+    void on_adjectives_catogory_button_clicked();
+
+    void on_verbs_category_button_clicked();
+
+private:
+    Ui::MainMenu *ui;
+    NounsMenu *nounsmenu;
+    AdjMenu *adjmenu;
+    VerbsMenu *verbsmenu;
+};
+
+#endif // MAINMENU_H
