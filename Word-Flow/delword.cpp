b697513d8b523d0b2fab75b562622bc32be8e7ff
timowka0304 <timoti20@mail.ru>
2019-06-25 02:40:02 +0700

    Added main func for DelWord

--- Word-Flow/delword.cpp
+++ Word-Flow/delword.cpp
@@ -4,6 +4,8 @@
 #include <QSqlQuery>
 #include <QSqlTableModel>
 #include <QDesktopWidget>
+#include <QDebug>
+#include <QMessageBox>
 
 DelWord::DelWord(QWidget *parent) :
     QDialog(parent),
@@ -31,17 +33,43 @@ void DelWord::Show_db()
     QSqlTableModel *model =new QSqlTableModel;
     model->setTable("UsersWords");
     model->setEditStrategy(QSqlTableModel::OnManualSubmit);
+    model->setFilter("valid == '1'");
+    model->removeColumn(0);
+    model->removeColumn(2);
     model->setHeaderData(0, Qt::Horizontal, QObject::tr("Английский"));
     model->setHeaderData(1, Qt::Horizontal, QObject::tr("Русский"));
     model->select();
     ui->tableView->setModel(model);
+    ui->tableView->setEditTriggers(QAbstractItemView::NoEditTriggers);
     ui->tableView->horizontalHeader()->setSectionResizeMode(0, QHeaderView::Stretch);
     ui->tableView->horizontalHeader()->setSectionResizeMode(1, QHeaderView::Stretch);
 }
 
 void DelWord::on_del_Button_clicked()
 {
+    QString choosen = ui->tableView->currentIndex().data(0).toString();
 
+    QSqlDatabase db = QSqlDatabase::addDatabase("QSQLITE");
+    db.setDatabaseName( "/home/timowka0304/Word-Flow/Word-Flow/UserDic.db3");
+    db.open();
+    QSqlQuery query;
+
+    query.exec(QStringLiteral("UPDATE UsersWords SET valid = 0 WHERE English = '%1' OR Russian = '%1'").arg(choosen));
+    QMessageBox msgBox;
+    msgBox.setText("Слова удалены из Вашего словаря!");
+    msgBox.setIcon(QMessageBox::Information);
+    msgBox.setStandardButtons(QMessageBox::Ok);
+    msgBox.setDefaultButton(QMessageBox::Ok);
+    int ret = msgBox.exec();
+       switch (ret) {
+       case QMessageBox::Ok:
+         Show_db();
+         msgBox.close();
+         break;
+      default:
+         msgBox.close();
+         break;
+    }
 }
 
 void DelWord::on_back_to_menu_Button_clicked()
