16967638b01f517c2f5bbd6e0f008fea6e778157
timowka0304 <timoti20@mail.ru>
2019-06-28 02:24:42 +0700

    Added icon

--- Word-Flow/delword.cpp
+++ Word-Flow/delword.cpp
@@ -15,6 +15,7 @@ DelWord::DelWord(QWidget *parent) :
     QDesktopWidget *pDescwidget=QApplication::desktop();
     move(pDescwidget->width()/2-width()/2, pDescwidget->height()/2-height()/2);
     this->setFixedSize(400, 300);
+    setWindowIcon(QIcon(":/new/prefix1/353a9a937bc4945eed556e5617806aab.png"));
     Show_db();
 }
 
