f4410b27e534864b5bea1070dc41829f1a4a4178
timowka0304 <timoti20@mail.ru>
2019-06-28 23:23:58 +0700

    Added StartFill-func

--- Word-Flow/testuser.cpp
+++ Word-Flow/testuser.cpp
@@ -39,3 +39,131 @@ void TestUser::on_back_to_menu_Button_clicked()
     this->close();
     emit UserDicMenu();
 }
+
+void TestUser::on_start_Button_clicked()
+{
+    srand(time(nullptr));
+    QMessageBox msgBox;
+    msgBox.setText("Вы уверены, что хотите начать тест?");
+    msgBox.setIcon(QMessageBox::Question);
+    msgBox.setStandardButtons(QMessageBox::Yes | QMessageBox::No);
+    msgBox.setDefaultButton(QMessageBox::Yes);
+    int ret = msgBox.exec();
+       switch (ret) {
+       case QMessageBox::No:
+         msgBox.close();
+         break;
+       case QMessageBox::Yes:{
+           QSqlDatabase db = QSqlDatabase::addDatabase("QSQLITE");
+           db.setDatabaseName( "/home/timowka0304/Word-Flow/Word-Flow/UserDic.db3");
+           db.open();
+           QSqlQuery query;
+           query.exec("SELECT valid FROM UsersWords WHERE valid = 1");
+           int size = 0 ;
+           while (query.next())
+               size++;
+           if (size > 10){
+               msgBox.close();
+               FillStart();
+               ui->back_to_menu_Button->setHidden(1);
+               ui->start_Button->setHidden(1);
+               ui->next_Button->setHidden(0);
+               ui->groupBox->setHidden(0);
+               ui->word_1->setHidden(0);
+               ui->word_2->setHidden(0);
+               ui->word_3->setHidden(0);
+               ui->word_4->setHidden(0);
+               //RunTest();
+           } else {
+               QMessageBox msgBox_w;
+               msgBox_w.setText("В словаре необходимо не менее 10 слов! Добавьте их, пожалуйста!");
+               msgBox_w.setIcon(QMessageBox::Warning);
+               msgBox_w.setStandardButtons(QMessageBox::Ok);
+               msgBox_w.setDefaultButton(QMessageBox::Ok);
+               int ret = msgBox_w.exec();
+                  switch (ret) {
+                  case QMessageBox::Ok:
+                    msgBox.close();
+                    break;
+                 default:
+                    msgBox.close();
+                    break;
+               }
+            }
+         break;}
+      default:
+         msgBox.close();
+         break;
+    }
+}
+
+void TestUser::FillStart(){
+    QSqlDatabase db = QSqlDatabase::addDatabase("QSQLITE");
+    db.setDatabaseName( "/home/timowka0304/Word-Flow/Word-Flow/UserDic.db3");
+    db.open();
+    QSqlQuery query;
+    query.exec("SELECT valid FROM UsersWords WHERE valid = 1");
+    int size = 0 ;
+    while (query.next())
+        size++;
+
+    srand(time_t(nullptr));
+    for(int i = 0; i < 10; i++){
+        numbers_words[i].question_number = rand() % 2 + 1;
+    }
+    for(int i = 0; i < 10; i ++){
+        numbers_words[i].word_answer_number = rand() % size + 1;
+        for(int j = 0; j < i; j ++){
+            if(numbers_words[j].word_answer_number == numbers_words[i].word_answer_number){
+                i --;
+                break;
+            }
+        }
+
+    }
+    for(int i = 0; i < 10; i++){
+        numbers_words[i].word_1_number = rand() % size + 1;
+        for(int j = 0; j < 10; j ++){
+            if(numbers_words[j].word_answer_number == numbers_words[i].word_1_number){
+                i --;
+                break;
+            }
+        }
+    }
+    for(int i = 0; i < 10; i++){
+        numbers_words[i].word_2_number = rand() % size + 1;
+        for(int j = 0; j < 10; j ++){
+            if(numbers_words[j].word_answer_number == numbers_words[i].word_2_number){
+                i --;
+                break;
+            }
+            if(numbers_words[i].word_1_number == numbers_words[i].word_2_number){
+                i --;
+                break;
+            }
+        }
+    }
+    for(int i = 0; i < 10; i++){
+        numbers_words[i].word_3_number = rand() % size + 1;
+        for(int j = 0; j < 10; j ++){
+            if(numbers_words[j].word_answer_number == numbers_words[i].word_3_number){
+                i --;
+                break;
+            }
+            if(numbers_words[i].word_1_number == numbers_words[i].word_3_number){
+                i --;
+                break;
+            }
+            if(numbers_words[i].word_2_number == numbers_words[i].word_3_number){
+                i --;
+                break;
+            }
+        }
+    }
+    for (int i = 0; i < 10; i++){
+        mas_answers[i] = 0;
+    }
+    for (int i = 0; i < 10; i++){
+        qDebug() <<  numbers_words[i].question_number << numbers_words[i].word_answer_number << numbers_words[i].word_1_number << numbers_words[i].word_2_number << numbers_words[i].word_3_number;
+    }
+}
--- Word-Flow/testuser.h
+++ Word-Flow/testuser.h
@@ -2,6 +2,16 @@
 #define TESTUSER_H
 
 #include <QDialog>
+#include <QString>
+#include <QSqlDatabase>
+
+struct Numbers_user{
+    int question_number;
+    int word_answer_number;
+    int word_1_number;
+    int word_2_number;
+    int word_3_number;
+};
 
 namespace Ui {
 class TestUser;
@@ -17,12 +27,25 @@ signals:
 public:
     explicit TestUser(QWidget *parent = nullptr);
     ~TestUser();
+    void RunTest();
+    void FillStart();
+    int mas_answers[10];
+    Numbers_user numbers_words[10];
+    int counter = 0;
+    void ResultShow();
+    void ENGtoRUS();
+    void RUStoENG();
+    void Warning(int flag, QString eng, QString rus);
+    int sum = 0;
 
 private slots:
     void on_back_to_menu_Button_clicked();
 
+    void on_start_Button_clicked();
+
 private:
     Ui::TestUser *ui;
+    QSqlDatabase db;
 };
 
 #endif // TESTUSER_H
