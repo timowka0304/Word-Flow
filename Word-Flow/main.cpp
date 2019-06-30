c9d8719baafa758c705022939019f21e3f487aef
timowka0304 <timoti20@mail.ru>
2019-07-01 02:35:56 +0700

    Added output test's logs in file

--- Word-Flow/main.cpp
+++ Word-Flow/main.cpp
@@ -1,12 +1,14 @@
 #include "mainwindow.h"
 #include <QApplication>
 #include <QtTest/QTest>
+#include <iostream>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
+    freopen("testing.log", "w", stdout);
     QTest::qExec(new Test_QLineEdit, argc, argv);
 
     return a.exec();
