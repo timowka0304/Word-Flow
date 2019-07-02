08207f1c59eb49650aace588898d19eb574f549e
timowka0304 <timoti20@mail.ru>
2019-07-02 14:04:07 +0700

    Fix int type

--- Word-Flow/adjmenu.cpp
+++ Word-Flow/adjmenu.cpp
@@ -58,7 +58,7 @@ void AdjMenu::on_dic_show_Button_clicked()
 
     while (query.next())
     {
-        if ((query.value(0) > 100) && (query.value(0) < 201)){
+        if ((query.value(0).toInt() > 100) && (query.value(0).toInt() < 201)){
             QString english = query.value(1).toString();
             QString russian = query.value(2).toString();
             ui->textBrowser->insertPlainText(english+" - "+russian+"\n");
