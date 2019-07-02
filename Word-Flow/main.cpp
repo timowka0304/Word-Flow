8b501a82ed058da5c7ecdb298c901f324f6a2110
timowka0304 <timoti20@mail.ru>
2019-07-02 23:10:17 +0700

    Added test

--- Word-Flow/main.cpp
+++ Word-Flow/main.cpp
@@ -8,8 +8,8 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
-    //freopen("/home/nikita/Рабочий стол/курсовая/Word-Flow/Word-Flow/testing.log", "w", stdout);
-    //QTest::qExec(new Test_QLineEdit, argc, argv);
+    freopen("testing.log", "w", stdout);
+    QTest::qExec(new Test_QLineEdit, argc, argv);
     //QTest::qExec(new test_radiobutton, argc, argv);
     return a.exec();
 }
